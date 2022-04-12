68b9e6149ba1a3117afa08de28b66fad7d5d020e
Sunny996 <satlaakash@gmail.com>
2022-04-12 18:30:08 +0530

    ADDED: A search method to search for an element in BST

--- src/main/MyBinaryTree.java
+++ src/main/MyBinaryTree.java
@@ -1,10 +1,23 @@
 package main;
 
 public class MyBinaryTree<K extends Comparable<K>> {
-    private MyBinaryNode<K> root;
+    public MyBinaryNode<K> root;
 
     public void add(K key) {
         this.root = this.addRecursively(root, key);
+    }
+
+    public boolean searchRecursively(MyBinaryNode<K> root, K key) {
+        MyBinaryNode<K> current = root;
+        if(current==null)
+            return false;
+        int compareResult = key.compareTo(current.key);
+        if(compareResult==0)
+            return true;
+        else if(compareResult<0)
+            return searchRecursively(current.left,key);
+        else
+            return searchRecursively(current.right,key);
     }
 
     private MyBinaryNode<K> addRecursively(MyBinaryNode<K> current, K key) {
@@ -26,5 +39,12 @@ public class MyBinaryTree<K extends Comparable<K>> {
 
     private int getSizeRecursive(MyBinaryNode<K> current) {
         return current == null ? 0 : 1 + this.getSizeRecursive(current.left) + this.getSizeRecursive(current.right);
+    }
+
+    @Override
+    public String toString() {
+        return "MyBinaryTree{" +
+                "root value=" + root.key +
+                '}';
     }
 }
